2b3a65656db2394046872e657326b17812810b8a
Beatriz <beasaddress@gmail.com>
2023-06-25 12:03:52 -0700

    added a table for departments and role. department table 'keys out' the id coloumn for later use. the role table refernces department id using foreignkey and primary key keywords. its also 'keying out' the id column for role table

--- db/schema.sql
+++ db/schema.sql
@@ -4,6 +4,17 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) UNIQUE NOT NULL
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) UNIQUE NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE role(
+    id INT NOT NULL AUTO_INCREMENT,
+    job_title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL,
+    PRIMARY KEY(id)
+
 );
