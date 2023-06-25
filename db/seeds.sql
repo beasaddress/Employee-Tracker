06c6ec670d0d4f2925a9731c439e2a72ce0b03be
Beatriz <beasaddress@gmail.com>
2023-06-25 12:20:04 -0700

    adding seeds into department table

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO department (name)
+VALUES ("Finance"),
+        ("Legal"),
+        ("Administration"),
+        ("Marketing"),
+        ("Tech"),
+        ("Board of Directors"),
+        ("HR");
