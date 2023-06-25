9edfb132c8c9941fe43c3a62f55219f2ae4400c3
Beatriz <beasaddress@gmail.com>
2023-06-25 12:56:53 -0700

    adding seeds

--- db/schema.sql
+++ db/schema.sql
@@ -6,12 +6,13 @@ USE employees_db;
 CREATE TABLE department(
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) UNIQUE NOT NULL,
+    manager VARCHAR(30) PRIMARY KEY,
     PRIMARY KEY(id)
 );
 
 CREATE TABLE role(
     id INT NOT NULL AUTO_INCREMENT,
-    job_title VARCHAR(30) NOT NULL,
+    job_title VARCHAR(30) NOT NULL PRIMARY KEY,
     salary DECIMAL,
     department_id INT,
     FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL,
@@ -23,8 +24,10 @@ CREATE TABLE employee(
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role _id VARCHAR(30),
-    manager_id INT,
+    role _id VARCHAR(30) NOT NULL,
+    manager_id VARCHAR(30),
     PRIMARY KEY (id)
-    FOREIGN KEY (role_id) REFERENCE role(job_title) ON DELETE SET NULL
+    FOREIGN KEY (role_id) REFERENCES role(job_title) ON DELETE SET NULL
+    FOREIGN KEY (manager_id) REFERENCES department(manager) ON DELETE SET NULL
 );
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,20 @@
-INSERT INTO department (name)
-VALUES ("Finance"),
-        ("Legal"),
-        ("Administration"),
-        ("Marketing"),
-        ("Tech"),
+INSERT INTO department (name, manager)
+VALUES ("Finance", "Michael Scott"),
+        ("Legal", "Creed Bratton"),
+        ("Administration", "Pam Beesly"),
+        ("Marketing", "Kelly Kapoor"),
+        ("Tech", "Jim Halpert"),
         ("Board of Directors"),
         ("HR");
+
+INSERT INTO role (job_title, salary, department_id)
+VALUES ("Accounts Payable", 65000, 1),
+        ("Legal Secretary", 60000, 2),
+        ("Receptionist", 38000, 3),
+        ("Social Media Manager", 70000, 4),
+        ("Software Engineer", 120000, 5),
+        ("CEO", 500000, 6),
+        ("HR Director", 80000, 7);
+
+INSERT INTO employee (first_name, last_name, role_id)
+        ("Angela", "Martin", 1, )
