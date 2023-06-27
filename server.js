818a058d1e402fac87ac69dabff29f01ec8475b0
Beatriz Sandoval <beasaddress@gmail.com>
2023-06-27 12:24:36 -0700

    adding a function for when the user selects to view the database. this should trigger another inquiry that will ask which table of the database to see, each choice will trigger a function that will trigger a db query function

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 //importing dependencies
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const PORT = process.env.PORT || 3001;
 
 //creating the connection to the database
 const db = mysql.createConnection(
     {
         host: 'localhost',
+        port: PORT,
         user: 'root',
         password: 'securesql',
         database: 'employees_db'
@@ -36,11 +38,39 @@ const db = mysql.createConnection(
                         break;
                     case "Exit":
                         console.log("Database closed. Goodbye.");
-                } testingSOmething() {
-                    
-                }
+                } 
             });
     }
 
+    //the functions will be triggered if the user selects any of the options that display
+    //when the user selects the "view option" at the start of the inquirer
+     function view(){
+        inquirer
+            .prompt([
+                {
+                    type: "list",
+                    name: "view",
+                    message: "You have selected to view our database. What would you like to view?",
+                    choices: ["Departments", "Roles", "Employees"]
+                }
+            ]).then(function(response){
+                switch(response.view){
+                    case "Departments":
+                        viewDepartments();
+                        break;
+                    case
+                }
+            })
+     }
 
+     function viewDepartments() {
 
+     }
+
+     function viewRoles() {
+
+     }
+
+     function viewEmployees() {
+
+     }
